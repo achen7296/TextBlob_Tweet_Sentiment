2d121ab69a67c4990eea03137fcdf8f3fd71faf5
Andrew <achen7296@gmail.com>
2021-07-15 13:59:26 -0400

    Implemented Flask, changed application to a web app

---
+++ server.py
@@ -0,0 +1,46 @@
+from initdb import *
+from client import client
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder="template")
+
+@app.route('/')
+def index():
+    #Checks if database is created
+    createDb()
+    return render_template("index.html")
+
+@app.route('/submit_button/',methods =["GET", "POST"])
+def execute():
+    # Check if integer was entered
+    if  request.method == 'POST':
+        print("Post request recieved")
+        try:
+            query = request.form["q"]
+            num = request.form["n"]
+            num = int(num)
+            print("These are the values for query and num " + str(query) + str(num))
+
+            # Parse num of tweets based on query into class
+            tweets = client(query, num)
+            users = tweets.dataHandler()
+
+            # Insert data into database
+            for user in users:
+                insertDb(user.date, user.name, user.tweet, user.getSentiment(), user.location)
+                print("Sucessfully placed " + str(user.name) + " into the database")
+
+            # Commit changes to database
+            conn.commit()
+
+            return render_template("index.html")
+
+        except ValueError:
+            print("Could not read query")
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
